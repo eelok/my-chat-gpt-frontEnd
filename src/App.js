fb27f883bf6f9ccf0ecf934233a07fc517cfa233
eelok <maria@resourceful-humans.de>
2023-04-15 17:42:11 +0200

    add fetch method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [prompt, updatePrompt] = useState(undefined);
+  const [loadin, setLoading] = useState(false);
+  const [answer, setAnswer] = useState(undefined);
+
+  const handleChange = (event) => {
+    updatePrompt(event.target.value);
+  };
+
+  const sendPrompt = async (event) => {
+    if (event.key !== 'Enter') {
+      return;
+    }
+    try {
+      setLoading(true);
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ prompt }),
+      };
+      const reply = await fetch('http://localhost:5500/ask', requestOptions);
+      if (!reply.ok) {
+        throw new Error('Something went wrong');
+      }
+      const { message } = await reply.json();
+      setAnswer(message);
+    } catch (err) {
+      console.log('err', err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="app">
       <div className="app-container">
@@ -9,10 +42,11 @@ function App() {
             type="text"
             className="spotlight__input"
             placeholder="Ask me anything...."
-            // style={{ backgroundImage: `urs(${lens})` }}
+            onChange={handleChange}
+            onKeyDown={(e) => sendPrompt(e)}
           />
           <div className="spotlight__answer">
-            Dubai is a desert city and has a warm and sunny climate throughout
+            {answer && <p>{answer}</p>}
           </div>
         </div>
       </div>
